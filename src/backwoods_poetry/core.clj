f5ae25f82af3444a327b6367b793266d1d5505cf
Lyon Lenk <thelyonshead@gmail.com>
2019-08-02 08:39:36 -0500

    parse int from Procfile

--- src/backwoods_poetry/core.clj
+++ src/backwoods_poetry/core.clj
@@ -4,9 +4,12 @@
             [compojure.handler :as handler]
             [ring.adapter.jetty :as jetty]))
 
+(defn parse-int [s]
+  (Integer. (re-find #"\d+" s)))
+
 (def handler
   (handler/site r/routes))
 
 (defn -main [PORT]
-  (jetty/run-jetty handler {:port PORT}))
+  (jetty/run-jetty handler {:port (parse-int PORT)}))
 
